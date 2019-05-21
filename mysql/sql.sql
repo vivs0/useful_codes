8f18b4b35fd2feb739015538121a07cf0265fd9d
Vivek Singh <vivek.bngl68@gmail.com>
2019-05-21 11:35:08 +0530

    Update sql.sql

--- mysql/sql.sql
+++ mysql/sql.sql
@@ -6,3 +6,8 @@ mysql_upgrade -u root -p
 
 #list all users
 select user from mysql.users;
+
+#mysql select top gainer daywise
+ select symbol, max(percentchange) as changes, date(`date`) as sDate from dailystockview
+ group by symbol, sDate
+ order by changes desc 
