863d0190e9104200beddbe355598e099113992b1
Vivek Singh <vivek.bngl68@gmail.com>
2019-08-14 17:29:31 +0530

    Update codes.js

--- javascript/codes.js
+++ javascript/codes.js
@@ -3,3 +3,10 @@
 function demo(btn){
     console.log(btn.parentNode.parentElement)
 }
+
+
+//get sibiling of clicked element
+function demo(btn){
+    $(btn.parentNode.parentElement.tagName+" td:nth-child(2)").text()
+}
+
