2655f0943620dbcd12bb7106cf4adf5e5c7b8c55
Vivek Singh <vivek.bngl68@gmail.com>
2020-01-09 12:40:59 +0530

    Update codes.js

--- javascript/codes.js
+++ javascript/codes.js
@@ -9,3 +9,23 @@ function demo(btn){
     $(btn.parentNode.parentElement.tagName+" td:nth-child(2)").text()
 }
 
+// function to convert form data to plain json
+function formSubmit(formid){
+	$.fn.formToJson = function(){
+		var json = {};
+		var raw = this.serializeArray();
+	    $.each(raw, function() {
+	        if (json[this.name] !== undefined) {
+	            if (!json[this.name].push) {
+	                json[this.name] = [json[this.name]];
+	            }
+	            json[this.name].push(this.value || '');
+	        } else {
+	            json[this.name] = this.value || '';
+	        }
+	    });
+	    return json;
+		
+	}
+	console.log(JSON.stringify($("#"+formid).formToJson()))
+}
